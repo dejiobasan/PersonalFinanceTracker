03b8bd040e83a817eeda88141cac31956c92fa65
dejiobasan <dejiobasan@yahoo.com>
2025-02-28 17:25:14 +0100

    enhance homepage layout with background image and update navbar icon color

--- Frontend/src/Components/Navbars/HomeNav.tsx
+++ Frontend/src/Components/Navbars/HomeNav.tsx
@@ -1,5 +1,5 @@
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { Wallet } from 'lucide-react';
 
 const HomeNav = () => {
@@ -7,14 +7,14 @@ const HomeNav = () => {
     <nav className="w-full flex justify-between lg:max-w-[90%] mx-auto px-8 py-12">
       <div>
         <Link to="/">
-          <Wallet color="blue" className="h-16 w-16" />
+          <Wallet color="white" className="h-16 w-16" />
         </Link>
       </div>
 
       <div className="w-[150px] flex justify-between items-center">
       <Link
           to='/register'
-          className="flex items-center bg-blue-950 text-white py-3 px-4 rounded-lg hover:bg-blue-900 transition-all duration-300"
+          className="flex items-center bg-blue-950 text-white py-3 px-4 rounded-lg hover:bg-blue-900 transition-all duration-300 mr-4"
         >
            Register
         </Link>
@@ -27,6 +27,6 @@ const HomeNav = () => {
       </div>
     </nav>
   )
-}
+};
 
-export default HomeNav
+export default HomeNav;
--- Frontend/src/Pages/Homepage.tsx
+++ Frontend/src/Pages/Homepage.tsx
@@ -1,13 +1,34 @@
-import HomeNav from "../Components/Navbars/HomeNav"
+import HomeNav from "../Components/Navbars/HomeNav";
+import shapeBg from "../Images/Shape.png";
 
 const Homepage = () => {
   return (
-    <div>
-      <header>
-          <HomeNav />
-      </header>
-    </div>
-  )
-}
+    <div className="relative h-screen bg-blue-500 flex flex-col overflow-hidden">
+      <div
+        className="absolute inset-0 bg-no-repeat bg-cover bg-center z-0"
+        style={{ backgroundImage: `url(${shapeBg})` }}
+      ></div>
 
-export default Homepage
+      <header className="relative z-10">
+        <HomeNav />
+      </header>
+
+      <div className="flex flex-col gap-20 flex-grow">
+        <main className="flex-1 flex items-center justify-center text-center z-40">
+          <div className="flex flex-col gap-6 w-2/3">
+            <h1 className="text-white text-[45px] text-h1 leading-tight tracking-wide font-extrabold mb-5">
+              Manage your Transactions <br />effortlessly
+            </h1>
+            <p className="text-white text-4xl">
+              The ultimate transaction management platform to safeguard your
+              finances <br />
+              and ensure you never miss a payment.
+            </p>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
