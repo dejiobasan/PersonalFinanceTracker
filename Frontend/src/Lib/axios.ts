c6a948103b41870f34378565ed2f27696870de67
dejiobasan <dejiobasan@yahoo.com>
2025-03-04 14:04:35 +0100

    update axios instance to use a fixed production base URL and remove development configuration

--- Frontend/src/Lib/axios.ts
+++ Frontend/src/Lib/axios.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:8000"
-      : "https://personalfinancetrackerbackend.onrender.com",
+  baseURL: "https://personalfinancetrackerbackend.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
