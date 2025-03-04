adedccbaf28ce6980ff541e47d82b3861b0207e4
dejiobasan <dejiobasan@yahoo.com>
2025-03-04 12:54:54 +0100

    update axios instance configuration for development and production environments, and set default content type header

--- Frontend/src/Lib/axios.ts
+++ Frontend/src/Lib/axios.ts
@@ -1,9 +1,14 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? 'https://personalfinancetrackerbackend.onrender.com' : "/api",
-    withCredentials: true,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:8000"
+      : "https://personalfinancetrackerbackend.onrender.com",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosInstance;
